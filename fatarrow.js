40b98082339c31effbd228ede1c2e0e51d6b09ca
dama220 <damaman9412@gmail.com>
2024-12-06 12:31:55 +0530

    new

---
+++ fatarrow.js
@@ -0,0 +1,25 @@
+
+
+//1. Function defination 
+
+function myfunctionA(d){
+    console.log('Hello '+d+' How  Are You');
+   
+}
+  
+//2. Function Calling
+
+myfunctionA('Damini Laddha');
+
+
+//1. Function Defination
+
+let myfunctionc = (name)=>{
+    console.log('Hello '+name+' How are you');
+}
+  
+//2. Function Calling
+    myfunctionc("Mishika Laddha");
+
+
+
